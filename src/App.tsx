3371652fb087f490b727d4730c32dca25f2a0fbb
yachu7 <yachumaharjan@gmail.com>
2024-04-29 22:57:49 +0545

    initial

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+ 
+
+  return (
+   
+      <div className='h-screen'>
+        
+          <Navbar />
+        
+      </div>
+    
+  )
+}
+
+export default App
