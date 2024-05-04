fe9aa1a84789e96015afdfed71af6d055416a0c9
yachu7 <yachumaharjan@gmail.com>
2024-05-04 16:02:20 +0545

    layout top and middle

--- src/components/Middle.tsx
+++ src/components/Middle.tsx
@@ -1,8 +1,87 @@
-
+import { AiOutlineEllipsis } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
 const Middle = () => {
   return (
-    <div>Middle</div>
-  )
-}
+    <div className="flex flex-row gap-2 bg-[#f5f5f5] ">
+      <div className="flex flex-col w-[63%] h-[40vh] bg-white ml-2 my-2">
+        <div className="flex flex-row justify-between m-2 ">
+          <p>Today Tasks</p>
+          <p>See All</p>
+        </div>
+        <div className="my-2 flex flex-row">
+          <div className="flex flex-col w-1/2 bg-[#f5f5f5] ml-2 rounded-3xl ">
+            <div className="flex flex-row items-center m-2 justify-between">
+              <p className="ml-2">Delivery App Kit</p>
+              <div className="p-2 rounded-full bg-white mr-2">
+                <AiOutlineEllipsis />
+              </div>
+            </div>
+            <p className="mx-2">
+              We got project to make a <br /> delivery ui kit called Foodnow...
+            </p>
+            <div className="m-2 flex flex-row ">
+              <div className="p-1 rounded-full bg-[#f5f5f5]">
+                <AiOutlineUser size={18} />
+              </div>
+              <div className="p-1 rounded-full bg-[#f5f5f5]">
+                <AiOutlineUser size={18} />
+              </div>
+              <div className="p-1 rounded-full bg-[#f5f5f5]">
+                <AiOutlineUser size={18} />
+              </div>
+              <div className="p-1 rounded-full bg-[#f5f5f5]">
+                <AiOutlineUser size={18} />
+              </div>
+              <p>+2</p>
+            </div>
+            <p className="flex justify-end mx-2">65%</p>
+            <div className="rounded-full m-2 ">
+              <div className="bg-green-500 p-2 w-[65%] rounded-l-full "></div>
+              </div>
+          </div>
+          <div className="flex flex-col w-1/2 bg-[#f5f5f5] ml-2 rounded-3xl mr-2">
+            <div className="flex flex-row items-center m-2 justify-between">
+              <p className="ml-2">Dribbble Shot</p>
+              <div className="p-2 rounded-full bg-white mr-2">
+                <AiOutlineEllipsis />
+              </div>
+            </div>
+            <p className="mx-2">
+              Make Dribbble shot with a <br /> project management theme...
+            </p>
+            <div className="m-2 flex flex-row ">
+              <div className="p-1 rounded-full bg-[#f5f5f5]">
+                <AiOutlineUser size={18} />
+              </div>
+              <div className="p-1 rounded-full bg-[#f5f5f5]">
+                <AiOutlineUser size={18} />
+              </div>
+              <div className="p-1 rounded-full bg-[#f5f5f5]">
+                <AiOutlineUser size={18} />
+              </div>
+              <div className="p-1 rounded-full bg-[#f5f5f5]">
+                <AiOutlineUser size={18} />
+              </div>
+              <p>+2</p>
+            </div>
+            <p className="flex justify-end mx-2">80%</p>
+            <div className="rounded-full m-2 ">
+              <div className="bg-green-500 p-2 w-[80%] rounded-l-full "></div>
+              </div>
+          </div>
+          
+        </div>
+        <div className="mx-2 p-2  text-white bg-neutral-900 rounded-full ">
+        <p className="ml-2">You Have Five tasks today.Keep it UP!</p>
 
-export default Middle
+        </div>
+
+        
+      </div>
+
+      <div className="bg-white m-2  w-[35%] ">b</div>
+    </div>
+  );
+};
+
+export default Middle;
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,11 +1,43 @@
-
+import {
+  AiOutlineSearch,
+  AiOutlineSetting,
+  AiOutlineBell,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { MdTune, MdOutlineWbSunny } from "react-icons/md";
 
 const Top = () => {
   return (
-    <div className="">
-        <input type="text" placeholder="Start Searching Here..."></input>
-    </div>
-  )
-}
+    <div className="p-4 flex items-center">
+      <div className="pl-4  p-4 bg-[#f5f5f5] rounded-l-full text-gray-500">
+        <AiOutlineSearch />
+      </div>
+      <input
+        type="text"
+        placeholder="Start Searching Here..."
+        className="p-3 w-[60%] bg-[#f5f5f5] border-none outline-none"></input>
+      <div className="p-4 bg-[#f5f5f5] rounded-r-full pr-4 text-gray-500">
+        <MdTune />
+      </div>
+      <div className="p-4 bg-[#f5f5f5] rounded-full ml-2 hover:brightness-95">
+        <MdOutlineWbSunny size={18} />
+      </div>
+      <div className="p-4 bg-[#f5f5f5] rounded-full ml-2 hover:brightness-95">
+        <AiOutlineBell size={18} />
+      </div>
+      <div className="p-4 bg-[#f5f5f5] rounded-full ml-2 hover:brightness-95">
+        <AiOutlineSetting size={18} />
+      </div>
 
-export default Top
+      <div className="p-4 bg-[#f5f5f5] rounded-full ml-2 ring-2 ring-green-500">
+        <AiOutlineUser size={18} />
+      </div>
+      <div className="flex flex-col ml-2">
+        <p className="font-semibold">Yachu Maharjan</p>
+        <p className="text-gray-500">Frontend-Developer</p>
+      </div>
+    </div>
+  );
+};
+
+export default Top;
